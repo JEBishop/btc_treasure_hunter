7c7ce153ce6f707b15033e2d9c2bf5ee4b51e23d
Josh Bishop <jsh.bshp@outlook.com>
2021-12-19 21:16:22 -0500

    Build wallet, get address, query for txns
    log to console if a non-zero balance is found

--- Models/Wallet.cs
+++ Models/Wallet.cs
@@ -3,14 +3,18 @@ using NBitcoin;
 
 namespace btc_treasure_hunter.Models {
     class Wallet {
-        String address;
-        float total_received;
-        float total_sent;
-        float balance;
-        float unconfirmed_balance;
-        float final_balance;
-        int n_tx;
-        int unconfirmed_n_tx;
-        int final_n_tx;
+        public PubKey public_key;
+        public BitcoinSecret private_key;
+        public KeyId public_key_hash;
+        public BitcoinPubKeyAddress mainnet_address;
+        public BitcoinSecret wif;
+
+        public Wallet(Key privateKey) {
+            private_key = privateKey.GetBitcoinSecret(Network.Main);
+            public_key = privateKey.PubKey;
+            public_key_hash = public_key.Hash;
+            mainnet_address = public_key_hash.GetAddress(Network.Main);
+            wif = privateKey.GetWif(Network.Main);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
 ﻿using System;
 using NBitcoin;
 using btc_treasure_hunter.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace btc_treasure_hunter
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
         {
-            Address addr = new Address(new Key());
-            Console.WriteLine("Private Key: " + addr.private_key.ToString());
-            Console.WriteLine("Public Key: " + addr.public_key); // 0251036303164f6c458e9f7abecb4e55e5ce9ec2b2f1d06d633c9653a07976560c
-            Console.WriteLine("Public Key Address: " + addr.public_key.GetAddress(ScriptPubKeyType.Legacy, Network.Main)); // 1PUYsjwfNmX64wS368ZR5FMouTtUmvtmTY
-            Console.WriteLine("Public Hash: " + addr.public_key.Hash); // f6889b21b5540353a29ed18c45ea0031280c42cf
-            Console.WriteLine("WIF: " + addr.wif);
+            while(1) {
+                Wallet wallet = new Wallet(new Key());
+                int balance = await sendRequest(wallet);
+                if(balance > 0) {
+                    Console.WriteLine("Private Key: " + wallet.private_key);
+                    Console.WriteLine("Public Key: " + wallet.public_key); 
+                    Console.WriteLine("Public Key Address: " + wallet.mainnet_address); 
+                    Console.WriteLine("Public Hash: " + wallet.public_key.Hash); 
+                    Console.WriteLine("WIF: " + wallet.wif);
+                }   
+            }
+        }
+
+        private static async Task<int> sendRequest(Wallet w) {
+            var stringTask = client.GetStringAsync("https://api.blockcypher.com/v1/btc/main/addrs/" + w.mainnet_address + "/balance");
+            var msg = await stringTask;
+            var responseObj = JObject.Parse(msg);
+            return responseObj.SelectToken("balance").Value<int>();
         }
     }
 }
